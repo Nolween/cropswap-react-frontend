cf4a458aafe8625e5a214bcef5e892028b294d6e
nolweenlopez <nolween.lopez@gmail.com>
2024-06-11 14:09:10 +0200

    Initial Commit: React + Tailwind Install To Do: First Component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="text-3xl text-blue-600">
+        CropSwap React Frontend
+    </div>
+  )
+}
+
+export default App
